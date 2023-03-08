b4dcd80b039cd46e9c51ee3f9611542d3982bc94
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-03-07 16:14:52 -0800

    resolver login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -25,8 +25,6 @@ const resolvers = {
         });
       },
 
-
-
       me: async (parent, context) => {
         if (context.user) {
           return User.findOne({ _id: context.user._id }).populate('thoughts');
@@ -60,8 +58,43 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in!');
 
-        }
+        },
 
+        //updateTicketStatus
+
+
+        //createComment
+
+
+        //createNote
+
+
+
+        //updateNote
+
+
+        //deleteNote
+
+        
+
+        //login
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('No user found with this email address');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
     }
 }
 
