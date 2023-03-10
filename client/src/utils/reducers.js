30ae8605cb44b6af5a3b306b0cf993f044123ed4
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-10 13:22:12 -0800

    create reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import {
     LOGIN,
     CREATE_USER
@@ -6,7 +7,24 @@ import {
 export default function reducer(state, action) {
     switch (action.type) {
         case LOGIN: {
-
+            return {
+                ...state,
+                token: action.payload.token,
+                user: action.payload.user
+            }
         }
+        case CREATE_USER: {
+            return {
+                ...state,
+                token: action.payload.token,
+                user: action.payload.user
+            }
+        }
+        default:
+            return state;
     }
+};
+
+export function useUserReducer(initialState) {
+    return useReducer(reducer, initialState);
 }
