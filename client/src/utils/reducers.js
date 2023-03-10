e8cea735151df7f6047dc5a36074ddcbf76aa376
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-10 11:19:08 -0800

    creating reducers

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,20 @@
+import {
+    GET_TICKET,
+    GET_USER_TICKETS,
+    CREATE_TICKET,
+    UPDATE_TICKET_STATUS,
+    CREATE_COMMENT,
+    CREATE_NOTE,
+    UPDATE_NOTE,
+    DELETE_NOTE,
+    LOGIN,
+    CREATE_USER
+} from "./actions";
+
+export default function reducer(state, action) {
+    switch (action.type) {
+        case GET_TICKET: {
+            
+        }
+    }
+}
