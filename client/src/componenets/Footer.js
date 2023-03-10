5d63d1c8169ec7938ecee2f401ec55fc727d6b2a
Megan Ellman <meganlellman@gmail.com>
2023-03-10 15:15:53 -0800

    created footer component

---
+++ client/src/componenets/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>
+                Made with &#127154; by Team 2
+                </p>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const secret = process.env.SECRET_KEY;
+// const secret = process.env.SECRET_KEY;
+const secret = 'mysecretssshhhhhhh';
 
 const expiration = '2h';
 
