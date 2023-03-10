62452f149e0f0dac34a096ec04f22d4b09b7c465
Megan Ellman <meganlellman@gmail.com>
2023-03-10 13:43:02 -0800

    added nav bar

---
+++ client/src/componenets/Header.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+
+function Header() {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
+    const [isActive, setActive] = useState("false");
+    const toggleClass = () => {
+        setActive(!isActive);
+    };
+    return (
+        <nav className='navbar'>
+            <div className='navbar-brand'>
+                <h1>Support Hero</h1>
+                <div role="button" class={isActive ? "is-active navbar-burger" : "navbar-burger"} onClick={toggleClass} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </div>
+            </div>
+
+            <div className={isActive ? "is-active navbar-menu" : "navbar-menu"}>
+                <div className='navbar-end'>
+                    <div className='navbar-item' >
+                        <Link to='/homepage'>Home</Link>
+                    </div>
+                    <div className='navbar-item'>
+                        <button onClick={logout}>Logout</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    )
+};
+
+export function Header;
