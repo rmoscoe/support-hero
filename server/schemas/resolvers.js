9fac50a99582e49dcdd0e81420fe5fea7752c76a
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-09 23:23:46 -0800

    populate user data for mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,135 +1,140 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Ticket, Comment } = require('../models');
 const { signToken } = require('../utils/auth');
-
+require('dotenv').config({ path: __dirname + '/../.env' });
 
 const resolvers = {
     Query: {
         // get Ticket by its ID
-        getTicketById: async (parent, {ticketId, userType}) => {
+        getTicketById: async (parent, { ticketId, userType }) => {
 
             if (userType == "Agent") {
-                return await Ticket.findOne({_id:ticketId}).populate('users').populate({path: 'comments', populate: {path: 'creator'}});
+                return await Ticket.findOne({ _id: ticketId }).populate('users').populate({ path: 'comments', populate: { path: 'creator' } });
             } else {
                 // TODO : if customer is viewing the ticket, dont show notes section
-                return await Ticket.findOne({_id:ticketId}).populate('users').populate({path: 'comments', select: ["message", "createdAt", "creator"], populate: {path: 'creator'}});
+                return await Ticket.findOne({ _id: ticketId }).populate('users').populate({ path: 'comments', select: ["message", "createdAt", "creator"], populate: { path: 'creator' } });
             }
-      },
+        },
 
-      // get Tickets by userId and Status
-      getTicketsByUserId: async (parent, {userId , status}) => {
-        if(status)
-            return await Ticket.find({
-            users : userId,
-            status : status
+        // get Tickets by userId and Status
+        getTicketsByUserId: async (parent, { userId, status }) => {
+            if (status)
+                return await Ticket.find({
+                    users: userId,
+                    status: status
 
-            }).populate('users').populate({path: "comments", populate: {path: "creator"}});
-        else 
-            return await Ticket.find({
-            users : userId,
-            }).populate('users').populate({path: "comments", populate: {path: "creator"}});
+                }).populate('users').populate({ path: "comments", populate: { path: "creator" } });
+            else
+                return await Ticket.find({
+                    users: userId,
+                }).populate('users').populate({ path: "comments", populate: { path: "creator" } });
 
-      },
+        },
     },
 
 
-    Mutation : {
+    Mutation: {
         //create new ticket
-        createTicket: async (parent, {title, description,priority}, context) => {
+        createTicket: async (parent, { title, description, priority }, context) => {
 
-            const usersDb = await User.find({type : "Agent"});
+            const usersDb = await User.find({ type: "Agent" });
 
-            const agentId = usersDb[Math.floor(Math.random * users.length)]._id;
+            const agentId = usersDb[Math.floor(Math.random() * usersDb.length)]._id;
 
-            const customerId = context.user._id ;
+            let customerId;
+            if (context.user) {
+                customerId = context.user._id;
+            } else {
+                customerId = process.env.CUSTOMER_ID;
+            }
 
             const users = [agentId, customerId]
 
-            if (context.user) {
-             const ticket = await Ticket.create({title, description,priority, users});
+            const ticket = await (await Ticket.create({ title, description, priority, users })).populate("users");
 
-             return ticket;
-            }
-            throw new AuthenticationError('You need to be logged in!');
+            return ticket;
+            // return Ticket.create({title, description, priority, users});
+            // }
+            // throw new AuthenticationError('You need to be logged in!');
 
         },
 
         //updateTicketStatus
-        updateTicketStatus: async (parent, {ticketId,status} , context) => {
+        updateTicketStatus: async (parent, { ticketId, status }, context) => {
             // if (context.user) {
-                const ticket = await Ticket.findOneAndUpdate(
-                    {_id : ticketId},
-                    {
-                        status : status
-                    },
-                    {
-                        new: true,
-                    }
-                )
-                return ticket;
+            const ticket = await Ticket.findOneAndUpdate(
+                { _id: ticketId },
+                {
+                    status: status
+                },
+                {
+                    new: true,
+                }
+            ).populate("users").populate({ path: "comments", populate: { path: "creator" } });
+            return ticket;
             // }
             // throw new AuthenticationError('You need to be logged in!');
         },
 
 
         //createComment
-        createComment: async (parent, {ticketId,message, userId}, context) => {
+        createComment: async (parent, { ticketId, message, userId }, context) => {
             // if (context.user) {
-                const comment = await Comment.create(
-                    {
-                        message,
-                        creator : userId
-                    }
-                )
-                await Ticket.findOneAndUpdate(
-                    { _id: ticketId },
-                    { $addToSet: { comments: comment._id } }
-                  );
-                return comment;
+            const comment = await Comment.create(
+                {
+                    message,
+                    creator: userId
+                }
+            )
+            await Ticket.findOneAndUpdate(
+                { _id: ticketId },
+                { $addToSet: { comments: comment._id } }
+            );
+            return comment;
             // } 
             // throw new AuthenticationError('You need to be logged in!');
         },
 
 
         //createNote
-        createNote: async (parent, {commentId,notes}, context) => {
+        createNote: async (parent, { commentId, notes }, context) => {
             // if (context.user) {
-                return await Comment.findOneAndUpdate(
-                    { _id: commentId },
-                    { 
-                         note : {notes}
-                    },
-                    {
-                        new: true,
-                        runValidators: true,
-                      }
-                  );
+            return await Comment.findOneAndUpdate(
+                { _id: commentId },
+                {
+                    note: { notes }
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            );
             // } 
             // throw new AuthenticationError('You need to be logged in!');
         },
 
         //updateNote
-        updateNote: async (parent,{commentId,notes}, context) => {
+        updateNote: async (parent, { commentId, notes }, context) => {
             // if(context.user){
-                return await Comment.findOneAndUpdate(
-                    {_id : commentId},
-                    {note : {notes}},
-                    {
-                        new: true,
-                        runValidators: true,
-                    }
-                )
+            return await Comment.findOneAndUpdate(
+                { _id: commentId },
+                { note: { notes } },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            )
             // }
             // throw new AuthenticationError('You need to be logged in!');
         },
 
         //deleteNote
-        deleteNote: async (parent,{commentId, notes}, context) => {
+        deleteNote: async (parent, { commentId, notes }, context) => {
             // if(context.user){
-                return await Comment.findOneAndDelete(
-                    {_id : commentId},
-                    {note : {notes}},
-                )
+            return await Comment.findOneAndDelete(
+                { _id: commentId },
+                { note: { notes } },
+            )
             // }
             // throw new AuthenticationError('You need to be logged in!');
         },
@@ -137,27 +142,27 @@ const resolvers = {
         //login
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
-      
-            if (!user) {
-              throw new AuthenticationError('No user found with this email address');
-            }
-      
-            const correctPw = await user.comparePassword(password);
-      
-            if (!correctPw) {
-              throw new AuthenticationError('Incorrect credentials');
-            }
-      
-            const token = signToken(user);
-      
-            return { token, user };
-          },
 
-          createUser: async (parent,{firstName, lastName, password, email}) => {
-            const user = await User.create({ firstName, lastName, password, email});
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+
+            const correctPw = await user.comparePassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        createUser: async (parent, { firstName, lastName, password, email }) => {
+            const user = await User.create({ firstName, lastName, password, email });
             const token = signToken(user);
             return { token, user };
-          }
+        }
     }
 }
 
