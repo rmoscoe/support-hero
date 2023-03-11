d86b1a8af5c3b98ede97fe74dd7834bfc23c39c7
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-03-10 18:58:23 -0800

    update form classname

--- client/src/components/CreateTicket.js
+++ client/src/components/CreateTicket.js
@@ -31,7 +31,7 @@ const CreateTicket = () => {
             <button>Open Create Ticket Modal</button>
 
             {/* Modal */}
-            <div className="modal">
+            <div className="modal create-form create-ticket-form">
                 <h2>Create Ticket</h2>
                 <button className="close-button" onClick={onClose}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M6.293 6.293a1 1 0 0 1 1.414 0L12 10.586l4.293-4.293a1 1 0 1 1 1.414 1.414L13.414 12l4.293 4.293a1 1 0 0 1-1.414 1.414L12 13.414l-4.293 4.293a1 1 0 0 1-1.414-1.414L10.586 12 6.293 7.707a1 1 0 0 1 0-1.414z"/></svg>
