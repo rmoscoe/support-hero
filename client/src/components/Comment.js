841b91c8e53c46947f0d0e91e67100e3b4177251
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-11 16:58:48 -0800

    complete JSX for Comment component

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import { useStoreContext } from "../utils/GlobalState";
 
 function Comment(props) {
+    const [state, dispatch] = useStoreContext();
+    const [userType, setUserType] = useState(state.user.type);
+
+    const handleEditButton = () => {
+
+    }
+
+    const handleDeleteButton = () => {
+
+    }
+
+    const addNote = () => {
+
+    }
 
     return props.comment.map((comment, idx) => (
         <div className="card" key={props.comment._id}>
@@ -12,15 +27,22 @@ function Comment(props) {
             <div className="card-content">
                 <div className="content">
                     <p>{props.comment.message}</p>
-                    {props.comment.creator.type === "Agent" && props.comment.note &&
+                    {userType === "Agent" && props.comment.note &&
                         <div className="card-content">
                             <div className="content columns">
                                 <p className="column is-four-fifths">{props.comment.note.notes}</p>
-                                <button className="button column is-link">
+                                <button className="button column is-link is-small" onClick={handleEditButton}>
                                     <i className="fa-solid fa-pencil"></i>
                                 </button>
+                                <button className="button column is-link is-small" onClick={handleDeleteButton}>
+                                    <i className="fa-solid fa-trash-can"></i>
+                                </button>
                             </div>
-                        </div>}
+                        </div>
+                    }
+                    {userType === "Agent" && !props.comment.note &&
+                        <button className="button is-link my-3 is-align-self-flex-end is-small" onClick={addNote}>Add Note</button>
+                    }
                 </div>
             </div>
         </div>
