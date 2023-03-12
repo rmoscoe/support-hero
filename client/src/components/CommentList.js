066884cfc73686f25bc07b6c5f5149ea7a68ad62
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-12 15:52:51 -0700

    fix bug notes not rendering

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -26,35 +26,38 @@ function CommentList(props) {
 
         try {
             const message = commentFormState.messageText;
-        document.getElementById("comment-message-textarea").value = "";
+            console.log(message);
+            document.getElementById("comment-message-textarea").value = "";
 
-        const { commentData } = await createComment({
-            variables: { ticketId, message, userId }
-        });
+            const { commentData } = await createComment({
+                variables: { ticketId, message, userId }
+            });
 
-        // window.location.reload(); // not sure whether this is necessary
+            // window.location.reload(); // not sure whether this is necessary
         } catch (error) {
             console.error(error);
         }
     }
 
     return (
-        < section className="section">
-            <h3 className="title is-3">Comment History</h3>
-            <Comment comments={props.comments} />
-            <form className="columns" onSubmit={handleCreateComment}>
-                <textarea
-                    name="messageText"
-                    rows="3"
-                    value={commentFormState.messageText}
-                    className="form-input w-100 column is-four-fifths"
-                    id="comment-message-textarea"
-                    placeholder="Add a comment..."
-                    onChange={handleCommentInput}
-                >
-                </textarea>
-                <input type="submit" className="button is-link column" value="Submit" />
-            </form>
+        <section>
+            <h3 className="title is-4 has-background-info-light mt-3 mb-1 p-2 has-text-info">Comment History</h3>
+            < section className="section">
+                <Comment comments={props.comments} />
+                <form className="columns box is-align-items-flex-end mt-3" onSubmit={handleCreateComment}>
+                    <textarea
+                        name="messageText"
+                        rows="3"
+                        value={commentFormState.messageText}
+                        className="form-input w-100 column is-four-fifths mx-3"
+                        id="comment-message-textarea"
+                        placeholder="Add a comment..."
+                        onChange={handleCommentInput}
+                    >
+                    </textarea>
+                    <input type="submit" className="button is-info column mx-3" value="Submit" />
+                </form>
+            </section>
         </section>
     );
 }
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,13 +1,15 @@
 import React, { createContext, useContext } from "react";
 import { useUserReducer } from "./reducers";
+import Auth from "./auth";
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
+const user = Auth.getUser();
 
 const StoreProvider = ({ value = [], ...props }) => {
     const [state, dispatch] = useUserReducer({
         // token: "",
-        user: {}
+        user: user.data
     });
 
     return <Provider value={[state, dispatch]} {...props} />;
