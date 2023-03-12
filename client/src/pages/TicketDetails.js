563ecb97a396c300c2c9e1fbeea7f85032cee1d7
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-03-11 18:22:38 -0800

    update class names for formatting on Ticket Details

--- client/src/pages/TicketDetails.js
+++ client/src/pages/TicketDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { GET_TICKET_BY_ID } from '../utils/queries';
+import CommentsList from '../components/CommentsList';
 import Auth from '../utils/auth';
 
 function TicketDetails() {
@@ -16,12 +17,25 @@ function TicketDetails() {
     
     return (
         <div>
-            <h2>{data.getTicketById.title}</h2>
-            <p>{data.getTicketById.description}</p>
-            <p>Priority: {data.getTicketById.priority}</p>
-            <p>Status: {data.getTicketById.status}</p>
-            <div className="container comment-container">
-                {/* <CommentList comments={data.comments} /> */}
+            <h2 className="title has-text-centered">{data.getTicketById.title}</h2>
+            <div className="block columns">
+                <div className="message is-info column has-text-centered is-half-tablet is-full-mobile">
+                    <p className="message-body">Status: <strong>{data.getTicketById.status}</strong></p>
+                </div>
+                <div className="message is-success column has-text-centered is-half-tablet is-full-mobile">
+                    <p className="message-body">Priority: <strong>{data.getTicketById.priority}</strong></p>
+                </div>
+            </div>
+            <div className="message is-info">
+                <div className="message-header">
+                    <p>Description</p>
+                </div>
+                <div className="message-body">
+                    <p>{data.getTicketById.description}</p>
+                </div>
+            </div>
+            <div className="container comments-container">
+                <CommentsList comments={data.getTicketById.comments} />
             </div>
         </div>
     );
