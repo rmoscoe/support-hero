d9e0139a66b9f3bd2766a14274a77d67eb8de856
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-12 17:17:11 -0700

    Revert "Revert "Feature/dark mode""

---
+++ client/src/Container.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+import App from './App';
+
+import ThemeProvider from './utils/ThemeContext';
+
+export default function Container() {
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+}
