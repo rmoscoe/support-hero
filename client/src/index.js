4351322602018336c910192c253659226aa846de
Megan Ellman <108302659+megellman@users.noreply.github.com>
2023-03-12 17:02:31 -0700

    Revert "Feature/dark mode"

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Container from './Container';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Container />
+    <App />
   </React.StrictMode>
 );
 
