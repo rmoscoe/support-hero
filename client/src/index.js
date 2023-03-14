234d7c8cad2a11b125b6adff5b99ff422a0122d9
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-03-14 11:12:50 -0700

    reformatted index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,14 +7,11 @@ import ThemeProvider from './utils/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider>
-    <App />
-  </ThemeProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
