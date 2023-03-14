1961a8a2f5301e686c87bb864ef79bc7f967a344
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-03-14 11:03:14 -0700

    reformatted TicketDetails.js

--- client/src/pages/TicketDetails.js
+++ client/src/pages/TicketDetails.js
@@ -10,30 +10,30 @@ import { useTheme } from '../utils/ThemeContext';
 function TicketDetails() {
     const { theme } = useTheme();
     if (!Auth.loggedIn()) window.location.assign('/login');
-    
+
     const { ticketId } = useParams();
 
     const { loading, error, data } = useQuery(GET_TICKET_BY_ID, {
-        variables: { ticketId , userType: Auth.getUser().data.type }
+        variables: { ticketId, userType: Auth.getUser().data.type }
     });
 
-    const [updateTicket , {status}] = useMutation(UPDATE_TICKET_STATUS);
+    const [updateTicket, { status }] = useMutation(UPDATE_TICKET_STATUS);
 
     if (Auth.getUser().data.type === 'Agent') {
-        
+
     }
 
     const updateTicketStatus = () => {
         updateTicket({
             variables: {
-                ticketId : ticketId,
-                status : "Closed"
+                ticketId: ticketId,
+                status: "Closed"
             }
         })
     }
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
-    
+
     return (
         <div>
             <h2 className={`${theme} title has-text-centered`}>{data.getTicketById.title}</h2>
@@ -49,7 +49,7 @@ function TicketDetails() {
                             <p className={`${theme}-secondary message-body is-size-5`}>Priority: <strong>{data.getTicketById.priority}</strong></p>
                         </div>
                     </div>
-                    { Auth.getUser().data.type === "Agent" && data.getTicketById.status !== "Closed" ? <button className={`${theme}-tertiary button`} onClick={updateTicketStatus}>Close Ticket</button> : <label></label> }
+                    {Auth.getUser().data.type === "Agent" && data.getTicketById.status !== "Closed" ? <button className={`${theme}-tertiary button`} onClick={updateTicketStatus}>Close Ticket</button> : <label></label>}
                 </div>
                 <div className={`${theme} message`}>
                     <div className={`message-header ${theme}-primary`}>
