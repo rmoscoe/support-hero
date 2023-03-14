88bbfab1dba6b3457362a12631d62a3d989b6bcb
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-03-14 11:08:57 -0700

    reformatted Container.js

--- client/src/Container.js
+++ client/src/Container.js
@@ -1,12 +1,12 @@
-import React  from 'react';
+import React from 'react';
 import App from './App';
 
 import ThemeProvider from './utils/ThemeContext';
 
 export default function Container() {
-  return (
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    );
 }
