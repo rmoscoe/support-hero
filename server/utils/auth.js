19f95ff818a94516b87e1ed215ae9b40c702779e
Megan Ellman <meganlellman@gmail.com>
2023-03-15 15:36:35 -0700

    if user is not logged in, navigated to login page

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,28 +6,28 @@ const secret = process.env.JWT_SECRET
 const expiration = '2h';
 
 module.exports = {
-    authMiddleware: function ({ req }) {
-        let token = req.body.token || req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-        if (req.headers.authorization) {
-            token = token.split(' ').pop().trim();
-        }
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
 
-        if (!token) {
-            return req;
-        }
+    if (!token) {
+      return req;
+    }
 
-        try {
-            const { data } = jwt.verify(token, secret, { maxAge: expiration });
-            req.user = data;
-        } catch {
-            console.log('Invalid token');
-        }
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
 
-        return req;
-    },
-    signToken: function ({ email, firstName, lastName, _id, type }) {
-        const payload = { email, firstName, lastName, _id, type };
-        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-    },
+    return req;
+  },
+  signToken: function ({ email, firstName, lastName, _id, type }) {
+    const payload = { email, firstName, lastName , _id, type };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
 };
