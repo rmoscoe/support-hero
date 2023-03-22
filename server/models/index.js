e33eb52e945cffba8ddc1272d57fd91e71dbe237
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-21 19:40:18 -0700

    create Email db model

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,6 @@ const Comment = require('./Comment');
 const Ticket = require('./Ticket');
 const User = require('./User');
 const Feedback = require('./Feedback');
+const Email = require('./Email');
 
 module.exports = { Comment, Ticket, User , Feedback };
