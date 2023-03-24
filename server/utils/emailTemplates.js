05ae85317f8e9ca72f8e9ba564fed03e434f2f61
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-23 17:29:01 -0700

    create email template for comment added by agent

--- server/utils/emailTemplates.js
+++ server/utils/emailTemplates.js
@@ -1,8 +1,10 @@
+const header = `<header style='align-items: stretch; display: flex; flex-shrink: 0; min-height: 3.25rem; font-family: BlinkMacSystemFont, -apple-system, Segoe UI, "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif;'>
+<h1 style="text-align: center; text-transform: uppercase; background-image: linear-gradient(-225deg, #002cb0 0%, #44107a 29%, #ff1361 67%, #fff800 100%); background-size: 200% auto; color: #fff; -webkit-background-clip: text; -webkit-text-fill-color: transparent; animation: textclip 2s linear infinite; display: inline-block; font-weight: 700 !important; font-size: 3rem !important; margin-block-start: 0.83em; margin-block-end: 0.83em; line-height: 1.5;">Support Hero</h1>
+</header>`;
+
 const customerSignupHtml = (firstName) => {
-    return `<header style='align-items: stretch; display: flex; flex-shrink: 0; min-height: 3.25rem; font-family: BlinkMacSystemFont, -apple-system, Segoe UI, "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif;'>
-        <h1 style="text-align: center; text-transform: uppercase; background-image: linear-gradient(-225deg, #002cb0 0%, #44107a 29%, #ff1361 67%, #fff800 100%); background-size: 200% auto; color: #fff; -webkit-background-clip: text; -webkit-text-fill-color: transparent; animation: textclip 2s linear infinite; display: inline-block; font-weight: 700 !important; font-size: 3rem !important; margin-block-start: 0.83em; margin-block-end: 0.83em; line-height: 1.5;">Support Hero</h1>
-    </header>
-    <main style="display: flex; font-family: BlinkMacSystemFont, -apple-system, Segoe UI, 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif; justify-content: center;">
+    return `${header}
+    <main style="display: flex; font-family: BlinkMacSystemFont, -apple-system, Segoe UI, 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif; justify-content: center; margin-top: 2rem;">
         <p>Hello ${firstName},</p>
         <br/>
         <h2 style="text-align: center, text-transform: uppercase: background-image: linear-gradient(-225deg, #002cb0 0%, #44107a 29%, #ff1361 67%, #fff800 100%); background-size: 200% auto; color: #fff; -webkit-background-clip: text; -webkit-text-fill-color: transparent; animation: textclip 2s linear infinite; font-weight: 700 !important; font-size: 2.25rem !important">Welcome to Support Hero!</h2>
@@ -19,3 +21,81 @@ const customerSignupHtml = (firstName) => {
         <p>The Support Hero Team</p>
     </main>`
 }
+
+const ticketCreatedHtml = (firstName, ticketId, ticketTitle, issueType, priority, description) => {
+    return `${header}
+    <main style="display: flex; font-family: BlinkMacSystemFont, -apple-system, Segoe UI, 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif; justify-content: center; margin-top: 2rem;">
+        <p>Hello ${firstName},</p>
+        <br/>
+        This email is to acknowledge that we have received your ticket for customer support, and you can expect a response within one business day. You may check the status of your ticket or add additional details at any time <a href="https://dry-fjord-88699.herokuapp.com/tickets/${ticketId}">here</a>.
+        <br/>
+        <section style="margin: 0 1.5rem 1rem 1.5rem; background-color: #ededed; border-radius: 0.25rem; padding: 1rem; box-shadow: 3px 3px 8px #787878;">
+            <h2 style="margin-bottom: 1em;">Ticket Details</h2>
+            <p><span style="font-weight:700;">Ticket Number: </span>${ticketId}</p>
+            <p><span style="font-weight:700;">Title: </span>${ticketTitle}</p>
+            <p><span style="font-weight:700;">Issue Type: </span>${issueType}</p>
+            <p><span style="font-weight:700;">Priority: </span>${priority}</p>
+            <br/>
+            <p><span style="font-weight:700;">Description: </span>${description}</p>
+        </section>
+        <br/>
+        <p>Regards,</p>
+        <br/>
+        <br/>
+        <p>The Support Hero Team</p>
+    </main>`
+}
+
+const commentAddedByAgentHtml = (firstName, ticketId, status, agentFirstName, commentDate, commentText) => {
+    return `<style scoped>
+        .comment-header {
+            color: #fff;
+            background-color: #5471d0;
+            border: 2px solid #5471d0;
+            box-shadow: 3px 3px 9px #2e4eb7;
+            border-radius: 5px;
+            padding-left: 0.75rem important!;
+            padding-right: 0.75rem important!;
+            margin: -0.75rem -0.75rem 075rem -0.75rem;
+        }
+        .comment-date {
+           display: block;
+           flex-basis: 0;
+           flex-grow: 1;
+           flex-shrink: 1;
+           padding: 0.75rem;
+           margin-block-start: 1em;
+           margin-block-end: 1em;
+           margin-inline-start: 0px;
+           margin-inline-end: 0px;
+           color: #fff;
+        }
+        @media screen and (min-width: 769px) {
+            .comment-header {
+                display: flex;
+            }
+            .comment-date {
+                text-align: right !important;
+            }
+        }
+    </style>
+    ${header}
+    <main style="display: flex; font-family: BlinkMacSystemFont, -apple-system, Segoe UI, 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif; justify-content: center; margin-top: 2rem;">
+        <p>Hello ${firstName},</p>
+        <br/>
+        An agent has added a comment to your ticket number <a href="https://dry-fjord-88699.herokuapp.com/tickets/${ticketId}>${ticketId}</a>. The status of your ticket has been updated to <span style="font-weight: 700">${status}</span>.
+        <br/>
+        <section style="background-color: #fff; box-shadow: 2px 2px 9 px rgb(84, 84, 84); border-radius: 5px; margin: 1.5rem !important;">
+            <header class="comment-header">
+                <p style="color: #fff; font-size: 1.25rem !important; display: block; flex-basis: 0; flex-grow: 1; flex-shrink: 1; padding: 0.75rem; font-weight: 700; margin-block-start: 1em; margin-block-end: 1em; margin-inline-start: 0px; margin-inline-end: 0px; line-height: 1.5;">${agentFirstName}</p>
+                <p class="comment-date">${commentDate}</p>
+            </header>
+            <div style="border-bottom-left-radius: 0.25rem; border-bottom-right-radius: 0.25rem; background-color: transparent; padding: 1.5rem; display: block; color: black;">
+                <div style="padding-left: 0.75rem !important; padding-right: 0.75rem!important; display: block; color: black;">
+                    <p style="display: block; margin-block-start: 1em; margin-block-end: 1em; margin-inline-start: 0px; margin-inline-end: 0px; color: black;">${commentText}</p>
+                </div>
+            </div>
+        </section>`
+}
+
+module.exports = { customerSignupHtml, commentAddedByAgentHtml, commentAddedByCustomerHtml, ticketClosedHtml, ticketCreatedHtml }
