af7071d4349becf06e3177ece8d70560ede5b3b9
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-24 20:08:58 -0700

    trying to fix bug heroku unable to load login page from email link

--- server/server.js
+++ server/server.js
@@ -23,13 +23,9 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-})
 
 async function verifyTransporter() {
     let transporter = await createTransporter();
