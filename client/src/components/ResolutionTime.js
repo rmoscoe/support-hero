e845a649f1d51d23e6723bf6bff0bd8a58b7b8d1
Megan Ellman <meganlellman@gmail.com>
2023-03-27 09:09:56 -0700

    added a message if no data is available

--- client/src/components/ResolutionTime.js
+++ client/src/components/ResolutionTime.js
@@ -3,16 +3,19 @@ import { useTheme } from '../utils/ThemeContext';
 
 function ResolutionTime({ metrics }) {
     const { theme } = useTheme();
+    console.log(metrics)
     // calculate average time between when ticket is created to when ticket is closed
     let total = 0;
     let closeCount = 0;
+
     for (let i = 0; i < metrics?.getTicketsByUserId.length; i++) {
         let createdAt = new Date(metrics?.getTicketsByUserId[i].createdAt);
         let closedAt = new Date(metrics?.getTicketsByUserId[i].closedAt);
         let timeInQueue = (closedAt - createdAt) / 86400000; //days
-        if(metrics?.getTicketsByUserId[i].status === 'Closed')closeCount++;
+        if (metrics?.getTicketsByUserId[i].status === 'Closed') closeCount++;
         total += isNaN(timeInQueue) ? 0 : timeInQueue;
     }
+
     let average = total / closeCount;
     // formats average in day/hour/minute/second format
     function formatTime(time) {
@@ -26,27 +29,36 @@ function ResolutionTime({ metrics }) {
         if (hours > 0) timeArr.push(`${hours} Hour${hours > 1 ? 's' : ''}`);
         if (minutes > 0) timeArr.push(`${minutes} Minute${minutes > 1 ? 's' : ''}`);
         if (seconds > 0) timeArr.push(`${seconds} Second${seconds > 1 ? 's' : ''}`);
-console.log(timeArr[0].split(' ')[0])
+        console.log(timeArr[0].split(' ')[0])
         return timeArr;
     }
 
+    function checkData(metrics) {
+        for (let i = 0; i < metrics?.getTicketsByUserId.length; i++) {
+            if (metrics?.getTicketsByUserId[i].closedAt === 'Invalid Date') {
+                return true
+            }
+            return false;
+        };
+    };
+
     return (
         <>
-            <div className={`metric-card ${theme}-secondary-bg card is-flex-grow-1`} style={{border: '1px solid black'}} >
-            <div className={`message-header ${theme}-quaternary`}>
-                <p className='description'>Average Resolution Time</p>
-            </div>
-            <div style={{display: 'flex', columnGap: '15px', justifyContent: 'center', marginTop: '30px'}}>
-               {formatTime(average).map((time, i) => (
-                    <div key={i} style={{textAlign: 'center', fontFamily: 'Bakbak One', fontSize: '35px'}}>              
-                    <p key={i}>{time.split(' ')[0]}</p>
-                    <p key={i + 1}>{time.split(' ')[1]}</p>
+            <div className={`metric-card ${theme}-secondary-bg card is-flex-grow-1`} style={{ border: '1px solid black' }} >
+                <div className={`message-header ${theme}-quaternary`}>
+                    <p className='description'>Average Resolution Time</p>
+                </div>
+                {checkData(metrics) ?
+                    <div style={{ display: 'flex', columnGap: '15px', justifyContent: 'center', marginTop: '30px' }}>
+                        {formatTime(average).map((time, i) => (
+                            <div key={i} style={{ textAlign: 'center', fontFamily: 'Bakbak One', fontSize: '35px' }}>
+                                <p key={i}>{time.split(' ')[0]}</p>
+                                <p key={i + 1}>{time.split(' ')[1]}</p>
+                            </div>
+                        ))}
                     </div>
-                ))} 
+                    : <p>No Data Available in Table</p>}
             </div>
-                
-            </div>
-
         </>
 
     )
