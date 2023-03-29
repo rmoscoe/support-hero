a0139790de11c29c6035afa1f07df498e0d5fab3
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-03-28 19:50:34 -0700

    add chat page and components

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,21 +1,16 @@
 import Auth from '../utils/auth';
-import { useEffect, useState } from 'react';
-import { useQuery, useMutation } from '@apollo/client';
-import { useTheme } from '../utils/ThemeContext';
-import { Navigate, useParams, useLocation } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { Navigate, useParams } from 'react-router-dom';
 import { GET_CHATROOM_BY_TICKET_ID } from '../utils/queries';
-import { io } from 'socket.io-client';
 import ChatHeader from '../components/ChatHeader';
 import ChatMessageList from '../components/ChatMessageList';
 import ChatForm from '../components/ChatForm';
 
 function Chat() {
     const userId = Auth.getUser().data._id;
-    const { theme } = useTheme();
     const { ticketId } = useParams();
-    // const socket = io();
 
-    const { loading, error, data , refetch } = useQuery(GET_CHATROOM_BY_TICKET_ID,
+    const { loading, error, data } = useQuery(GET_CHATROOM_BY_TICKET_ID,
         {
             variables: { ticketId }
         });
