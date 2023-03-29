755a804e659e8a831bdc06cd3769c51c3a73bae8
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-03-28 18:38:02 -0700

    update models for chat

--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,7 @@ const Ticket = require('./Ticket');
 const User = require('./User');
 const Feedback = require('./Feedback');
 const Email = require('./Email');
+const ChatRoom = require('./ChatRoom');
+const ChatMessage = require('./ChatMessage');
 
-module.exports = { Comment, Ticket, User , Feedback };
+module.exports = { Comment, Ticket, User , Feedback, ChatRoom, ChatMessage };
